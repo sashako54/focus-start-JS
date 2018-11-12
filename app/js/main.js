dff9f3cfe265501878439334f570e1318d124de7
Sashako54 <sashako54@yandex.ru>
2018-11-12 20:44:41 +0700

    рандомно выводятся продукты

--- app/js/main.js
+++ app/js/main.js
@@ -42,3 +42,66 @@ var light = (function() {
 
 // light.setLightInterval();
 // light.delLightInterval();
+
+var randomPackages = (function() {
+
+	function getRandomInteger(min, max) {
+		// в выражении не добавляем 1, так как консоль ругается, когда рандомно выпадает последний элемент
+		return Math.floor( min + Math.random() * (max - min));
+	}
+
+	var package = [
+		{
+			'url':'assets/images/shot-1.jpg',
+			'h': 'Стандартный пакет',
+			'time':'08 апреля 2012',
+		},
+		{
+			'url':'assets/images/shot-2.jpg',
+			'h': 'Новый ЦФТ банк',
+			'time':'09 сентября 2016',
+		},
+		{
+			'url':'assets/images/shot-3.jpg',
+			'h': 'Каталог разработок',
+			'time':'03 марта 2015',
+		}
+	]
+
+	function getRandomPackage() {
+		var elem = package[getRandomInteger(0, 2)];
+		return elem;
+	}
+	
+	function addRandomPackage() {
+		var package = getRandomPackage();
+		var wrapper = document.getElementsByClassName('o-main__pockets')[0];
+		// создаём элементы HTML
+		var elemWrapper = document.createElement('div');
+		var elemImg = document.createElement('a');
+		var elemTitle = document.createElement('h4');
+		var elemTime = document.createElement('time');
+		// Добавляем стили
+		elemWrapper.classList.add('o-main__pocket', 'l-grid-4');
+		elemImg.classList.add('o-main__pocket-img');
+		elemTitle.classList.add('o-main__pocket-title');
+		elemTime.classList.add('o-main__pocket-date');
+		// Добавляем содержимое
+		elemImg.style.backgroundImage = `url(${package.url}`;
+		elemTitle.innerHTML = package.h;
+		elemTime.innerHTML = package.time;
+		// содержимое добавляем внутрь обёртки
+		elemWrapper.appendChild(elemImg);
+		elemWrapper.appendChild(elemTitle);
+		elemWrapper.appendChild(elemTime);
+		// выводим на страницу
+		wrapper.appendChild(elemWrapper);
+	}
+	return {
+		addRandomPackage: addRandomPackage,
+	}
+})();
+
+randomPackages.addRandomPackage();
+randomPackages.addRandomPackage();
+randomPackages.addRandomPackage();
