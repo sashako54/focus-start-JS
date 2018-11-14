411a5f81e7b4d1ac7b00bfbe4a9845a2e9f5be4a
Sashako54 <sashako54@yandex.ru>
2018-11-14 21:31:46 +0700

    начал делать реализацию карусели

--- app/js/main.js
+++ app/js/main.js
@@ -52,16 +52,37 @@ var randomPackages = (function() {
 
 	var packageObj = [
 		{
+			'name': 'package-1',
 			'url':'assets/images/shot-1.jpg',
 			'h': 'Стандартный пакет',
 			'time':'08 апреля 2012',
 		},
 		{
+			'name': 'package-2',
 			'url':'assets/images/shot-2.jpg',
 			'h': 'Новый ЦФТ банк',
 			'time':'09 сентября 2016',
 		},
 		{
+			'name': 'package-3',
+			'url':'assets/images/shot-3.jpg',
+			'h': 'Каталог разработок',
+			'time':'03 марта 2015',
+		},
+		{
+			'name': 'package-4',
+			'url':'assets/images/shot-1.jpg',
+			'h': 'Стандартный пакет',
+			'time':'08 апреля 2012',
+		},
+		{
+			'name': 'package-5',
+			'url':'assets/images/shot-2.jpg',
+			'h': 'Новый ЦФТ банк',
+			'time':'09 сентября 2016',
+		},
+		{
+			'name': 'package-6',
 			'url':'assets/images/shot-3.jpg',
 			'h': 'Каталог разработок',
 			'time':'03 марта 2015',
@@ -79,10 +100,12 @@ var randomPackages = (function() {
 			var package = getRandomPackage();
 			var template = document.querySelector('#packageTemplate');
 	
+			var wrapperPackage = template.content.getElementsByClassName('o-main__pocket')[0];
 			var elemImg = template.content.querySelectorAll('a.o-main__pocket-img')[0];
 			var elemTitle = template.content.querySelectorAll('h4.o-main__pocket-title')[0];
 			var elemTime = template.content.querySelectorAll('time.o-main__pocket-date')[0];
 	
+			wrapperPackage.setAttribute(data-name, 'hello');
 			elemImg.style.backgroundImage = `url(${package.url}`;
 			elemTitle.innerHTML = package.h;
 			elemTime.innerHTML = package.time;
@@ -92,10 +115,32 @@ var randomPackages = (function() {
 			wrapper.appendChild(clone);
 		}
 	}
-	
+
 	return {
 		addRandomPackages: addRandomPackages,
 	}
 })();
 
 randomPackages.addRandomPackages();
+
+
+var slider = (function() {
+	var buttonLeft = document.getElementsByClassName('js-slider-button-left')[0];
+	var buttonRight = document.getElementsByClassName('js-slider-button-right')[0];
+	
+	function moveCarouselLeft() {
+		var elem = document.getElementsByClassName('js-packages-carousel')[0];
+		// добавляем в конец первый элемент
+		console.log(elem.children[0].dataset)
+		elem.appendChild(elem.children[0]);
+	}
+
+	function moveCarouselRight() {
+		var elem = document.getElementsByClassName('js-packages-carousel')[0];
+		// добавляем в начало последний элемент
+		elem.insertBefore(elem.children[elem.children.length - 1], elem.children[0]);
+	}
+
+	buttonLeft.addEventListener('click', moveCarouselLeft);
+	buttonRight.addEventListener('click', moveCarouselRight);
+})()
