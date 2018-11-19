02f9c718d7955085ef6d603e145516c8211ac0a5
sashako54 <sashako54@yandex.ru>
2018-11-19 14:03:53 +0700

    парсим и получаем url параметр id

---
+++ app/js/getUrlParam.js
@@ -0,0 +1,13 @@
+function getUrlParam(param) {
+	var fullUrl = window.location.search.substring(1);
+	var paramArray = fullUrl.split('&');
+	for ( var i = 0; i < paramArray.length; i++ ) {
+		var currentParam = paramArray[i].split('=');
+		if ( currentParam[0] == param ) {
+			console.log(currentParam[1])
+			return currentParam[1];
+		}
+	}
+}
+
+export { getUrlParam };
---
+++ app/js/second.js
@@ -0,0 +1,13 @@
+import { getUrlParam } from '/js/getUrlParam.js';
+
+const xhr = new XMLHttpRequest();
+let packageObj = {};
+
+xhr.open("GET", "http://localhost:3000/api/app_packages.json", true);
+
+xhr.send();
+
+xhr.onload = function(e) {
+	packageObj = JSON.parse(xhr.responseText);
+	getUrlParam('id');
+}
